e43d49b9892c3f7d8736d37e5530b1fde63390a7
thiagocabralcorreia <thiagocabralcorreia@gmail.com>
2023-02-06 14:17:46 -0300

    Add bank statement listing by date

--- src/index.js
+++ src/index.js
@@ -103,4 +103,19 @@ app.post("/withdraw", checkForAccountNINO, (request, response) => {
   return response.status(201).send();
 });
 
+app.get("/statement/date", checkForAccountNINO, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  );
+
+  return response.json(customer.statement);
+});
+
 app.listen("3333");
