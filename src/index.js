325f8dee7265e4251ca71cde2a175cc56e7e87ff
thiagocabralcorreia <thiagocabralcorreia@gmail.com>
2023-02-06 12:23:46 -0300

    Add account deposit

--- src/index.js
+++ src/index.js
@@ -60,4 +60,21 @@ app.get("/statement", checkForAccountNINO, (request, response) => {
   return response.json(customer.statement);
 });
 
+app.post("/deposit", checkForAccountNINO, (request, response) => {
+  const { description, amount } = request.body;
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
 app.listen("3333");
